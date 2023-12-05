4321581fc3499d79118d43ea65b90034f687e81c
HP Power <alfe.devenecia.up@phinmaed.com>
2023-12-05 13:33:42 +0100

    Innit commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AlponsPyroDeVenecia-Block5-Quiz"
+include(":app")
+ 
